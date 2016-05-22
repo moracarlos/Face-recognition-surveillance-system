981cd08f8573cecd5c10963b8d9f0a71e2bcd55c
moracarlos <moracarlos9@gmail.com>
2016-05-22 15:52:57 -0400

    RPi camera

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ static domain_name_servers=192.168.0.1
 
 reboot
 
-Para instalar dependencias:
+#Para instalar dependencias:
 
-opencv
+#opencv
 sudo apt-get install build-essential
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 git clone https://github.com/Itseez/opencv.git
@@ -26,9 +26,18 @@ cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j7
 sudo make install
 
-crossbar
+#crossbar
 sudo apt-get install python-pip
+sudo pip install pip==8.1.1
+sudo apt-get install libffi-dev
+sudo apt-get install libssl-dev
 pip install crossbar
 
 nodejs
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#imutils
+pip install imutils
+
 DEBUG=admin:* npm start //Para correr el administrador con la plantilla generada por express
--- recognizer/src/camera.py
+++ recognizer/src/camera.py
@@ -4,18 +4,37 @@ import base64
 class Camera(object):
     'Hace conexion con la camara, obtiene frames y diferencia entre la camara de la RPi o web'
 
-    def __init__(self):
+    def __init__(self, rpi=False):
         #aca se deberia determinar si estamos en una RPi o una camara web
-        self.video = cv2.VideoCapture(0)
+        if rpi:
+            from picamera.array import PiRGBArray
+            from picamera import PiCamera
+            self.camera = PiCamera()
+            self.camera.resolution = (320,240)
+            self.camera.framerate(32)
+            self.camera.start_preview
+            self.rawCapture = PiRGBArray(camera, size(320,240))
+        else:
+            self.video = cv2.VideoCapture(0)
+
     
     def __del__(self):
-        self.video.release()
+        if rpi:
+            self.camera.stop_preview()
+        else:
+            self.video.release()
 
     def get_frame(self):
         'Obtiene un frame con formato html para ser enviado al browser'
-        success, frame = self.video.read()
-        frame = cv2.resize(frame, (320, 240), None, cv2.INTER_CUBIC)
-        return frame
+        if rpi:
+            frame = self.camera.capture(self.rawCapture, format='bgr', use_video_port=True)
+            frame = frame.array
+            return frame
+        else:
+
+            success, frame = self.video.read()
+            frame = cv2.resize(frame, (320, 240), None, cv2.INTER_CUBIC)
+            return frame
 
     def encode_frame(self, frame):
         'Codifica el frame a base64'
